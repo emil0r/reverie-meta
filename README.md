687786c1166690e482a4cedf135927a0c5539768
Emil Bengtsson <emil@emil0r.com>
2016-08-28 08:11:35 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# reverie-meta
+
+reverie module for page meta data
+
+## License
+
+Copyright © 2016 Emil Bengtsson
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
+
+---
+
+Coram Deo
---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject reverie-meta "0.1.0-SNAPSHOT"
+  :description "reverie module for page meta data"
+  :url "http://reveriecms.org"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+
+                 ;; reverie
+                 [reverie-core "0.8.3-alpha3"]
+                 [reverie-sql "0.8.3-alpha3"]])
